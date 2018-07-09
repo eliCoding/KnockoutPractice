188af2529038ee6f62a6a24b3f1d565e4d1070b2
Elmira <elmira.amanollahi@enlivenfinancial.com>
2018-07-09 15:54:25 -0700

    computed Observables

--- KnockoutMVVM/KnockoutMVVM/App_Start/BundleConfig.cs
+++ KnockoutMVVM/KnockoutMVVM/App_Start/BundleConfig.cs
@@ -22,13 +22,16 @@ namespace KnockoutMVVM
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
                            "~/Scripts/knockout.js",
-                                 "~/Scripts/home.js"
+                                 "~/Scripts/home.js",
+                                   "~/Scripts/data.js"
 
                       ));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                            "~/Content/style.css"
+                      ));
         }
     }
 }
--- KnockoutMVVM/KnockoutMVVM/Scripts/home.js
+++ KnockoutMVVM/KnockoutMVVM/Scripts/home.js
@@ -1,20 +1,58 @@
-﻿$(function () {
+﻿   $(function () {
+            var photoPath = "/images/";
 
+            // function helper 
+            my.formatCurrency = function (value) {
+                return "$" + value.toFixed(2);
+            };
 
+            // for creating Product Models
+            my.Product = function () {
+                this.id = ko.observable();
+                this.salePrice = ko.observable();
+                this.photo = ko.observable();
+                this.shortDescription = ko.observable();
+                this.photoUrl = ko.computed(function () {
+                    return photoPath + this.photo();
+                }, this);
+            };
 
-    var product = {
+      
+            // The ViewModel
+            my.vm = {
+                metadata: {
+                    pageTitle: "Knockout: Computed - Read/Write Converter",
+                    personal: {
+                        link: "",
+                        text: ""
+                    }
+                },
+                product: ko.observable(
+                    new my.Product()
+                        .shortDescription("Taylor Koa Series K66ce")
+                        .salePrice(4199)
+                        .photo("Taylor Koa Series K66ce Grand Symphony 12-String Cutaway Acoustic Electric Guitar.png")
+                    ),
+                quantity: ko.observable(4)
+            };
 
-        id: ko.observable(1001),
-        itemNumber: ko.observable("T314CE"),
-        model: ko.observable("Taylor 314ce"),
-        salesPrice: ko.observable(1199.95)
+            ///////////////////////////////////////////////////
+            // read/write computed
+            ///////////////////////////////////////////////////
+            my.vm.extendedPrice = ko.computed({
+                read: function () {
+                    var extPrice = this.product() ?
+                       this.product().salePrice() * parseInt("0" + this.quantity(), 10) : 0;
+                    return '$' + extPrice.toFixed(2);
+                },
+                write: function (value) {
+                    value = parseFloat(value.replace(/[^\.\d]/g, ""));
+                    value = isNaN(value) ? 0 : value;
+                    var unitPrice = value / this.quantity();
+                    this.product().salePrice(unitPrice);
+                },
+                owner: my.vm
+            });
 
-
-    };
-
-    ko.applyBindings(product);
-
-
-
-}
-)
+            ko.applyBindings(my.vm);
+        });
